72a4d0708a1af7b05975aa77e119533a565a8493
Railly Hugo <railly_2009@hotmail.com>
2024-06-30 20:11:14 -0500

    fix: address obsdian conventions

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
-import { Plugin } from "obsidian";
-import { RevealMethodSettingsTab } from "settings/reveal-method";
-import { marked } from "marked";
+import {
+	Plugin,
+	MarkdownRenderer,
+	PluginSettingTab,
+	App,
+	Setting,
+} from "obsidian";
 
 interface SimpleFlashcardsPluginSettings {
 	toggleRevealMethod: "surface-click" | "button-click" | "hover";
@@ -19,156 +23,148 @@ export default class SimpleFlashcardsPlugin extends Plugin {
 
 		this.registerMarkdownCodeBlockProcessor(
 			"flashcard",
-			async (source, el, ctx) => {
-				const lines = source
-					.split("\n")
-					.filter((line) => line.length > 0);
-
-				const flashcardContainer = el.createDiv({
-					cls: "flashcard-container",
-				});
-
-				const flashcardEl = flashcardContainer.createDiv({
-					cls: "flashcard",
-				});
-				const frontFace = flashcardEl.createDiv({
-					cls: "flashcard-face flashcard-face--front",
-				});
-				const backFace = flashcardEl.createDiv({
-					cls: "flashcard-face flashcard-face--back",
-				});
-
-				if (lines[0].startsWith("Q:")) {
-					const questionParts = [] as string[];
-					const answerParts = [] as string[];
-					let isAnswer = false;
-
-					lines.forEach((line) => {
-						if (line.startsWith("Q:")) {
-							questionParts.push(line.substring(2).trim());
-							isAnswer = false;
-						} else if (line.startsWith("A:")) {
-							if (answerParts.length > 0) {
-								answerParts[answerParts.length - 1] += "  ";
-							}
-							answerParts.push(line.substring(2).trim());
-							isAnswer = true;
-						} else if (isAnswer) {
-							answerParts.push(line.trim());
-						}
-					});
-
-					const question = questionParts.join(" ");
-					const answer = answerParts.join("  \n");
-
-					frontFace.insertAdjacentHTML(
-						"afterbegin",
-						await marked(question, { async: true })
-					);
-					backFace.insertAdjacentHTML(
-						"afterbegin",
-						await marked(answer, { async: true })
-					);
-				} else {
-					let frontContent = lines.join(" ");
-					let backContent = lines.join(" ");
-
-					frontContent = this.getClozeFront(frontContent);
-					backContent = this.getClozeBack(backContent);
-
-					frontFace.insertAdjacentHTML(
-						"afterbegin",
-						await marked(frontContent, { async: true })
-					);
-					const answerContainer = backFace.createDiv({
-						cls: "answer-container",
-					});
-					answerContainer.insertAdjacentHTML(
-						"afterbegin",
-						await marked(backContent, {
-							async: true,
-							gfm: true,
-							breaks: true,
-						})
-					);
-				}
-
-				switch (this.settings.toggleRevealMethod) {
-					case "hover":
-						flashcardContainer.addEventListener(
-							"mouseenter",
-							() => {
-								flashcardEl.addClass("is-flipped");
-							}
-						);
-						flashcardContainer.addEventListener(
-							"mouseleave",
-							() => {
-								flashcardEl.removeClass("is-flipped");
-							}
-						);
-						break;
-
-					case "surface-click":
-						flashcardEl.addClass("clickable");
-						flashcardContainer.addEventListener(
-							"click",
-							(event) => {
-								//@ts-ignore
-								if (event.target.closest("img")) {
-									event.stopPropagation();
-								} else {
-									flashcardEl.toggleClass(
-										"is-flipped",
-										!flashcardEl.hasClass("is-flipped")
-									);
-								}
-							}
-						);
-						break;
-
-					case "button-click":
-					default: {
-						const revealButton = frontFace.createEl("button", {
-							cls: "flashcard-button",
-							text: "Reveal",
-						});
-						revealButton.addEventListener("click", () => {
-							flashcardEl.addClass("is-flipped");
-						});
-
-						const hideButton = backFace.createEl("button", {
-							cls: "flashcard-button",
-							text: "Hide",
-						});
-						hideButton.addEventListener("click", () => {
-							flashcardEl.removeClass("is-flipped");
-						});
-						break;
-					}
-				}
+			(source, el, ctx) => {
+				this.renderFlashcard(source, el, ctx);
 			}
 		);
 
-		const observer = new MutationObserver((mutations) => {
-			mutations.forEach((mutation) => {
-				if (
-					mutation.type === "childList" &&
-					mutation.addedNodes.length > 0
-				) {
-					this.adjustFlashcardHeight();
-				}
-			});
+		this.registerDomEvent(document, "DOMContentLoaded", () => {
+			this.adjustFlashcardHeight();
 		});
 
-		const config = { childList: true, subtree: true };
+		this.registerEvent(
+			this.app.workspace.on("resize", () => {
+				this.adjustFlashcardHeight();
+			})
+		);
+	}
 
-		const targetNode = this.app.workspace.containerEl;
-		observer.observe(targetNode, config);
+	async renderFlashcard(source: string, el: HTMLElement, ctx: any) {
+		const lines = source.split("\n").filter((line) => line.length > 0);
+		const flashcardContainer = el.createDiv({ cls: "flashcard-container" });
+		const flashcardEl = flashcardContainer.createDiv({ cls: "flashcard" });
+		const frontFace = flashcardEl.createDiv({
+			cls: "flashcard-face flashcard-face--front",
+		});
+		const backFace = flashcardEl.createDiv({
+			cls: "flashcard-face flashcard-face--back",
+		});
+
+		if (this.isClozeFlashcard(lines)) {
+			await this.renderClozeContent(
+				lines.join("\n"),
+				frontFace,
+				backFace,
+				ctx
+			);
+		} else {
+			const { question, answer } = this.parseQAFormat(lines);
+			await MarkdownRenderer.render(
+				this.app,
+				question,
+				frontFace,
+				ctx.sourcePath,
+				this
+			);
+			await MarkdownRenderer.render(
+				this.app,
+				answer,
+				backFace,
+				ctx.sourcePath,
+				this
+			);
+		}
+
+		this.setupRevealMethod(
+			flashcardContainer,
+			flashcardEl,
+			frontFace,
+			backFace
+		);
+	}
+
+	isClozeFlashcard(lines: string[]): boolean {
+		return lines.some((line) => line.includes("{{") && line.includes("}}"));
+	}
+
+	parseQAFormat(lines: string[]): { question: string; answer: string } {
+		let question = "";
+		let answer = "";
+		let isAnswer = false;
+
+		lines.forEach((line) => {
+			if (line.startsWith("Q:")) {
+				question += line.substring(2).trim() + "\n\n";
+			} else if (line.startsWith("A:")) {
+				answer += line.substring(2).trim() + "\n\n";
+				isAnswer = true;
+			} else if (isAnswer) {
+				answer += line + "\n";
+			} else {
+				question += line + "\n";
+			}
+		});
+
+		return {
+			question: question.trim(),
+			answer: answer.trim(),
+		};
+	}
+
+	setupRevealMethod(
+		flashcardContainer: HTMLElement,
+		flashcardEl: HTMLElement,
+		frontFace: HTMLElement,
+		backFace: HTMLElement
+	) {
+		switch (this.settings.toggleRevealMethod) {
+			case "hover":
+				flashcardContainer.addEventListener("mouseenter", () => {
+					flashcardEl.addClass("is-flipped");
+				});
+				flashcardContainer.addEventListener("mouseleave", () => {
+					flashcardEl.removeClass("is-flipped");
+				});
+				break;
+
+			case "surface-click":
+				flashcardEl.addClass("clickable");
+				flashcardContainer.addEventListener("click", (event) => {
+					if ((event.target as HTMLElement).closest("img")) {
+						event.stopPropagation();
+					} else {
+						flashcardEl.toggleClass(
+							"is-flipped",
+							!flashcardEl.hasClass("is-flipped")
+						);
+					}
+				});
+				break;
+
+			case "button-click":
+			default: {
+				const revealButton = frontFace.createEl("button", {
+					cls: "flashcard-button",
+					text: "Reveal answer",
+				});
+				revealButton.addEventListener("click", () => {
+					flashcardEl.addClass("is-flipped");
+				});
+
+				const hideButton = backFace.createEl("button", {
+					cls: "flashcard-button",
+					text: "Hide answer",
+				});
+				hideButton.addEventListener("click", () => {
+					flashcardEl.removeClass("is-flipped");
+				});
+				break;
+			}
+		}
 	}
 
 	onunload() {
-		console.log("unloading plugin");
+		console.log("Unloading Simple Flashcards plugin");
 	}
 
 	async loadSettings() {
@@ -186,16 +182,40 @@ export default class SimpleFlashcardsPlugin extends Plugin {
 	getClozeFront(content: string): string {
 		const clozeRegex = /{{(.*?)}}/g;
 		return content.replace(clozeRegex, (_, word) => {
-			const underscoreText = "_".repeat(word.length);
-			return underscoreText;
+			return "&#95;".repeat(word.length);
 		});
 	}
 
 	getClozeBack(content: string): string {
 		const clozeRegex = /{{(.*?)}}/g;
-		return content.replace(clozeRegex, (_, hiddenText, __) => {
+		return content.replace(clozeRegex, (_, hiddenText) => {
 			return `<span class='cloze-reveal'>${hiddenText}</span>`;
 		});
+	}
+
+	async renderClozeContent(
+		content: string,
+		frontFace: HTMLElement,
+		backFace: HTMLElement,
+		ctx: any
+	) {
+		const frontContent = this.getClozeFront(content);
+		const backContent = this.getClozeBack(content);
+
+		await MarkdownRenderer.render(
+			this.app,
+			frontContent,
+			frontFace,
+			ctx.sourcePath,
+			this
+		);
+		await MarkdownRenderer.render(
+			this.app,
+			backContent,
+			backFace,
+			ctx.sourcePath,
+			this
+		);
 	}
 
 	adjustFlashcardHeight() {
@@ -203,11 +223,52 @@ export default class SimpleFlashcardsPlugin extends Plugin {
 			const frontFace = flashcard.querySelector(".flashcard-face--front");
 			const backFace = flashcard.querySelector(".flashcard-face--back");
 
-			const frontHeight = frontFace ? frontFace.scrollHeight : 0;
-			const backHeight = backFace ? backFace.scrollHeight : 0;
+			if (
+				frontFace instanceof HTMLElement &&
+				backFace instanceof HTMLElement
+			) {
+				const frontHeight = frontFace.scrollHeight;
+				const backHeight = backFace.scrollHeight;
 
-			const maxHeight = Math.max(frontHeight, backHeight);
-			flashcard.addClass(`height-${maxHeight}`);
+				const maxHeight = Math.max(frontHeight, backHeight);
+				(flashcard as HTMLElement).style.height = `${maxHeight}px`;
+			}
 		});
+	}
+}
+
+class RevealMethodSettingsTab extends PluginSettingTab {
+	plugin: SimpleFlashcardsPlugin;
+
+	constructor(app: App, plugin: SimpleFlashcardsPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		containerEl.createEl("h2", { text: "Simple flashcards settings" });
+
+		new Setting(containerEl)
+			.setName("Reveal method")
+			.setDesc("Choose how to reveal the answer on flashcards")
+			.addDropdown((dropdown) =>
+				dropdown
+					.addOption("surface-click", "Click on flashcard")
+					.addOption("button-click", "Click on button")
+					.addOption("hover", "Hover over flashcard")
+					.setValue(this.plugin.settings.toggleRevealMethod)
+					.onChange(
+						async (
+							value: SimpleFlashcardsPluginSettings["toggleRevealMethod"]
+						) => {
+							this.plugin.settings.toggleRevealMethod = value;
+							await this.plugin.saveSettings();
+						}
+					)
+			);
 	}
 }
